eef6bbded30251122e69e911d2c8f9f908ed3c97
Derek Huang <djh458@stern.nyu.edu>
2022-02-27 20:00:45 -0500

    main.cc: add code to show window, .gitignore: ignore build, add one-touch build and top-level CMakeLists.txt

--- src/main.cc
+++ src/main.cc
@@ -3,14 +3,23 @@
  * @brief Main entry point for app.
  */
 
+#include <QtCore/QSize>
 #include <QtGui/QColor>
 #include <QtGui/QPalette>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
 
 int main(int argc, char **argv)
 {
   const QPalette palette = QPalette(QColor("55557f"));
   QApplication app = QApplication(argc, argv);
   app.setPalette(palette);
+  QMainWindow main_window = QMainWindow();
+  // make main_window non-resizable
+  const QSize main_window_size = QSize(500, 686);
+  main_window.resize(main_window_size);
+  main_window.setMinimumSize(main_window_size);
+  main_window.setMaximumSize(main_window_size);
+  main_window.show();
   return app.exec();
 }
