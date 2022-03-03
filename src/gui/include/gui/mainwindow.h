1d4de040eee9ddad5c3d9498556e76400b831227
Derek Huang <djh458@stern.nyu.edu>
2022-03-03 03:04:13 -0500

    mainwindow.h: fill in some QObject fields

--- src/gui/include/gui/mainwindow.h
+++ src/gui/include/gui/mainwindow.h
@@ -5,8 +5,15 @@
  * @copyright MIT License
  */
 
+#include <memory>
+
 #include <QtCore/Qt>
+#include <QtGui/QAction>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
 #ifndef PWR_MAINWINDOW_H_
@@ -16,9 +23,30 @@ class MainWindow: public QMainWindow
 {
 public:
   MainWindow(QWidget * = nullptr);
-  ~MainWindow();
+  QLabel &image_label() const;
+  QMenuBar &menu_bar() const;
+  QMenu &file_menu() const;
+  QMenu &settings_menu() const;
+  QMenu &help_menu() const;
+  QAction &save_profile_action() const;
+  QAction &load_profile_action() const;
+  QAction &config_action() const;
+  QAction &prefs_action() const;
+  QAction &key_shortcuts_action() const;
+  QAction &about_action() const;
+  QStatusBar &status_bar() const;
+
 private:
-  
+  std::unique_ptr<QLabel> image_label_;
+  std::unique_ptr<QMenu> file_menu_;
+  std::unique_ptr<QMenu> settings_menu_;
+  std::unique_ptr<QMenu> help_menu_;
+  std::unique_ptr<QAction> save_profile_action_;
+  std::unique_ptr<QAction> load_profile_action_;
+  std::unique_ptr<QAction> config_action_;
+  std::unique_ptr<QAction> prefs_action_;
+  std::unique_ptr<QAction> key_shortcuts_action_;
+  std::unique_ptr<QAction> about_action_;
 };
 
 #endif // PWR_MAINWINDOW_H_
